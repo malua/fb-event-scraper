cb57fec0fe680baa00230f99b6cd923021504f67
Lukas Mayr <lukas.mayr@posteo.at>
2019-09-06 22:19:27 +0200

    removed mail functionality

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
-const port = process.env.PORT
-const watchdogEnabled = process.env.WATCHDOG_ENABLE === 'true'
-const watchdogInterval = parseInt(process.env.WATCHDOG_INTERVAL) || 86400000
-const watchdogEventUrl = process.env.WATCHDOG_EVENT_URL
-const watchdogScrapUrl = `http://localhost:${port}/scrap/`
-const watchdogExpectedResult = !watchdogEnabled ? {} : require(`../${process.env.WATCHDOG_EXPECTED_FILE}`)
-console.log('watchdogInterval', watchdogInterval)
+const port = process.env.PORT;
+const watchdogEnabled = process.env.WATCHDOG_ENABLE === "true";
+const watchdogInterval = parseInt(process.env.WATCHDOG_INTERVAL) || 86400000;
+const watchdogEventUrl = process.env.WATCHDOG_EVENT_URL;
+const watchdogScrapUrl = `http://localhost:${port}/scrap/`;
+const watchdogExpectedResult = !watchdogEnabled
+  ? {}
+  : require(`../${process.env.WATCHDOG_EXPECTED_FILE}`);
+console.log("watchdogInterval", watchdogInterval);
 
 export default {
   port,
@@ -12,9 +14,5 @@ export default {
   watchdogInterval,
   watchdogEventUrl,
   watchdogScrapUrl,
-  watchdogExpectedResult,
-  mailgunKey: process.env.MAILGUN_KEY,
-  mailgunDomain: process.env.MAILGUN_DOMAIN,
-  mailgunSender: process.env.MAILGUN_SENDER,
-  mailgunRecipient: process.env.MAILGUN_RECIPIENT
-}
+  watchdogExpectedResult
+};
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
-import initExpress from './initExpress'
-import initApi from './initApi'
-import initMailer from './initMailer'
+import initExpress from "./initExpress";
+import initApi from "./initApi";
 
-const start = async (config) => {
-  const app = await initExpress(config)
-  await initApi(app, config)
-  const mailer = initMailer(config)
+const start = async config => {
+  const app = await initExpress(config);
+  await initApi(app, config);
 
-  const wd = require('./watchdog')(config)
+  const wd = require("./watchdog")(config);
   wd.start((diff, html) => {
-    console.error('!!! result mismatched !!!')
-    console.log(diff)
-    mailer.send(html)
-  })
-}
+    console.error("!!! result mismatched !!!");
+    console.log(diff);
+  });
+};
 
 export default {
   start
-}
+};
